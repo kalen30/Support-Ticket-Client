29dc3f5d9ca2d01a82af12a9b8a89b102a9c0981
kalen30 <kalen30@uw.edu>
2025-06-24 14:10:55 -0700

    more dup code

--- src/App.jsx
+++ src/App.jsx
@@ -17,17 +17,3 @@ function App() {
 }
 
 export default App
-
-import TicketForm from './TicketForm'
-import TicketList from './TicketList'
-
-function App() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-100 to-gray-300 py-10">
-      <TicketForm />
-      <TicketList />
-    </div>
-  )
-}
-
-export default App
