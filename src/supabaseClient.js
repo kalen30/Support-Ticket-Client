3e8ad70975d75695de2e99758654f1a05f2db2dd
kalen30 <kalen30@uw.edu>
2025-06-23 18:36:08 -0700

    added to vite

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -7,7 +7,7 @@
  */
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://utuiturpazgxejaqqdkv.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0dWl0dXJwYXpneGVqYXFxZGt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NjU3NzAsImV4cCI6MjA2NjA0MTc3MH0.wIsPpy1RQqtB3yyzO30pry2AxhE30oocrMUzKe7FbN8'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
