5426c057b8e6192471fecae5a871a94cecfd1383
kalen30 <kalen30@uw.edu>
2025-06-24 14:19:32 -0700

    changed the vite names

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -7,7 +7,7 @@
  */
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_URL
+const supabaseAnonKey = import.meta.env.VITE_ANON
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
